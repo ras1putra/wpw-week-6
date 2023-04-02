c26e57f0843d2fd8862dbb0397046d6738eaebf9
Putra Yanuar Rasyid <111832174+ras1putra@users.noreply.github.com>
2023-04-02 18:09:49 +0700

    Initial commit

---
+++ src/components/HomeForm.vue
@@ -0,0 +1,111 @@
+<template>
+    <div>{{ pegawai }}</div>
+    <div class="m-0 p-4 bg-gray-100">
+        <div class="m-4 p-4 bg-white rounded-md shadow-md">
+            <div class="justify-between flex">
+                <div class="text-2xl font-light text-slate-600">
+                    Pekerja Baru
+                </div>
+                <button
+                    class="flex items-center px-2 py-1 border-slate-400 border-2 rounded-[5px] hover:bg-slate-700 hover:text-white"
+                    @click="showClass">
+                    <img src="src/assets/plus.svg" class="w-4 mr-2 bg-white rounded-full">
+                    Pekerja Baru
+                </button>
+            </div>
+            <form>
+                <div v-show="show"
+                    class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-100 bg-gray-200 w-[600px] text-slate-600 rounded-xl shadow-xl">
+                    <div class="p-4 text-center text-xl font-bold mt-6 text-black">
+                        Tambah Pekerja Baru
+                    </div>
+                    <div class="mx-4 p-4 flex justify-between font-bold items-center">
+                        <label>Nama Lengkap</label>
+                        <input type="text" placeholder="Nama Lengkap" class="w-3/4 px-4 py-2">
+                    </div>
+                    <div class="mx-4 p-4 flex justify-between font-bold items-center">
+                        <label>Jenis Kelamin</label>
+                        <div class="w-3/4 space-x-2">
+                            <input type="radio" id="laki">
+                            <label for="laki">Laki-laki</label>
+                            <input type="radio" id="per">
+                            <label for="per">Perempuan</label>
+                        </div>
+                    </div>
+                    <div class="mx-4 p-4 flex justify-between font-bold items-center">
+                        <label for="umur">Umur</label>
+                        <input type="number" name="umur" placeholder="Umur" class="w-3/4 px-4 py-2">
+                    </div>
+                    <div class="mx-4 p-4 flex justify-between font-bold items-center">
+                        <label for="posisi">Posisi</label>
+                        <div class="w-3/4">
+                            <select name="posisi" id="posisi" placeholder="Posisi yang Dilamar" class="px-4 py-2">
+                                <option value="1">Head Marketing</option>
+                                <option value="2">Sales Assistan</option>
+                                <option value="3">Head Timekeeper</option>
+                                <option value="4">Tugas Teroos</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="mx-4 p-4 flex justify-between font-bold items-center">
+                        <label for="date">Tanggal Masuk</label>
+                        <input type="date" name="date" class="w-3/4 px-4 py-2">
+                    </div>
+                    <div class="flex mx-8 p-4 items-center space-x-6 font-bold justify-end mt-6">
+                        <button class="bg-green-600 px-4 py-2 text-white rounded-md" type="submit">
+                            Simpan
+                        </button>
+                        <button type="button" class="bg-red-600 px-6 py-2 text-white rounded-md" @click="closeShow">
+                            Batal
+                        </button>
+                    </div>
+                </div>
+            </form>
+            <div class="pt-8">
+                <div class="p-2 py-4 text-white bg-cyan-500 font-bold grid grid-cols-12 text-center">
+                    <div>No.</div>
+                    <div class="col-span-2">Nama</div>
+                    <div>Jenis Kelamin</div>
+                    <div class="col-span-2">Umur</div>
+                    <div class="col-span-2">Posisi</div>
+                    <div class="col-span-2">Tanggal Masuk</div>
+                </div>
+                <div class="p-2 bg-gray-200 grid grid-cols-12 text-center items-center">
+                    <div class="col-span-1">1</div>
+                    <div class="col-span-2 text-left">Paidi</div>
+                    <div class="col-span-1">L</div>
+                    <div class="col-span-2">12</div>
+                    <div class="col-span-2">sales</div>
+                    <div class="col-span-2">12-01-2020</div>
+                    <div>
+                        <button class="font-bold text-white bg-green-600 rounded-md py-2 w-16 hover:opacity-40">
+                            Edit
+                        </button>
+                    </div>
+                    <div class="col-span-1">
+                        <button class="font-bold text-white bg-red-600 rounded-md py-2 w-16 hover:opacity-40">
+                            Hapus
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            show: false,
+        };
+    },
+    methods: {
+        showClass() {
+            this.show = true;
+        },
+        closeShow() {
+            this.show = false;
+        },
+    }
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import './style.css'
+import HomeForm from './components/HomeForm.vue'
+
+createApp(HomeForm).mount('#app')
